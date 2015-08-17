5560a5ca754d4518621e1e826b312a419c91067d
alex <alexis1784@gmail.com>
2015-08-17 14:21:08 +0500

    урок  https://github.com/yiisoft/yii2/blob/master/docs/guide-ru/start-databases.md завершен

---
+++ basic/controllers/CountryController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 17.08.15
+ * Time: 13:45
+ */
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use yii\data\Pagination;
+use app\models\Country;
+
+class CountryController extends Controller
+{
+    public function actionIndex()
+    {
+        $query = Country::find();
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 5,
+            'totalCount' => $query->count(),
+        ]);
+
+        $countries = $query->orderBy('name')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index', [
+            'countries' => $countries,
+            'pagination' => $pagination,
+        ]);
+    }
+}
---
+++ basic/models/Country.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 17.08.15
+ * Time: 13:41
+ */
+
+namespace app\models;
+use yii\db\ActiveRecord;
+
+class Country extends ActiveRecord
+{
+
+}
