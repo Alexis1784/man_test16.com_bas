8d111f6d0f7430f37045c4e96f454f111c4b02f7
alex <alexis1784@gmail.com>
2015-08-17 23:10:34 +0500

    mygreate_view_in_view - рендерим вид внутри другого вида

--- basic/views/site/mygreate_view_in_view.php
+++ basic/views/site/mygreate_view_in_view.php
@@ -3,10 +3,9 @@
  * Created by PhpStorm.
  * User: alex
  * Date: 17.08.15
- * Time: 18:24
+ * Time: 23:06
  */
 use yii\helpers\Html;
 ?>
 
-<?= Html::encode($ssv) ?>
-<?= $this->render('mygreate_view_in_view') ?>
+<?= Html::encode('dsf') ?>
