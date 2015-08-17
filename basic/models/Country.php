cf7b26468113742b75d0c0ff1f897062d0cf8839
alex <alexis1784@gmail.com>
2015-08-17 18:00:06 +0500

    ready for studing view

---
+++ basic/models/Country.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 17.08.15
+ * Time: 17:56
+ */
+
+namespace app\models;
+use yii\db\ActiveRecord;
+
+class Country extends ActiveRecord
+{
+
+}
