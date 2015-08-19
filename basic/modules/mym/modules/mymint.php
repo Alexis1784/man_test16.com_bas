f563c4fbed0a26c519146e908036009b83841fef
alex <alexis1784@gmail.com>
2015-08-20 02:46:00 +0500

    int module

---
+++ basic/modules/mym/modules/mymint.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\mym\modules;
+
+class mymint extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\mym\modules\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
